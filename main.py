712e1b187baf29a3c3b4c877975efa946b657918
connor <cmcummings2003@gmail.com>
2019-04-12 13:27:59 -0500

    Program is functional

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # main.py
 from tkinter import Tk
-import time
+from time import time
 from config import *
 from visualizer import Visualizer
 from controlpanel import ControlPanel
+from algorithms import *
+from utils import random_list
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+current_milli_time = lambda: int(round(time() * 1000))
 
 class Main(Tk):
     """The main class."""
+
+    ALGORITHMS = {
+        "Bubble": BubbleSort,
+        "Bogo": BogoSort
+    }
 
     def __init__(self):
         # Tk init
@@ -19,13 +26,14 @@ class Main(Tk):
 
         # Sorting control variables
         self.sorting = False
+        data = random_list(0, 100, 200)
+        self.sort = list(self.ALGORITHMS.values())[0](data) # First algorithm
 
         self.sort_delay = 1000/DEFAULT_SORT_SPEED # delay, in milliseconds
         self.tick_timer = current_milli_time()
 
         # Visualizer init
         self.visualizer = Visualizer(self, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bd=0, highlightthickness=0)
-        self.visualizer.draw_data(0)
 
         # Control panel init
         self.control_panel = ControlPanel(self)
@@ -38,15 +46,36 @@ class Main(Tk):
         if self.sorting:
             if current_milli_time() - self.tick_timer > self.sort_delay: # Apply sorting delay
                 # Sort
-                print("sort loop")
+                self.sort.advance_sort()
+                print(self.sort.__class__.__name__, "Iteration:", self.sort.iterations)
                 self.tick_timer = current_milli_time()
-
+            
+        self.visualizer.draw_data(self.sort.data, self.sort.cursor, self.sort.sorted)
+        
+        
         # Tk mainloop()
         self.update_idletasks()
         self.update()
 
+    def new_algo(self, algo, min_val, max_val, size):
+        data = random_list(min_val, max_val, size)
+        self.sort = self.ALGORITHMS[algo](data)
+        self.visualizer.algo_name = algo
+        self.pause()
 
+    def pause(self):
+        self.control_panel.b_pauseplay.config(text="Play")
+        self.sorting = False
+    
+    def play(self):
+        self.control_panel.b_pauseplay.config(text="Pause")
+        self.sorting = True
 
+    def pauseplay(self):
+        if self.sorting:
+            self.pause()
+        else:
+            self.play()
 
 
 
